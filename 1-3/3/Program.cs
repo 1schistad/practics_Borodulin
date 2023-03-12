b7514cca921813d1327676bb5c86eda4e4b345ba
1schistad <125075398+1schistad@users.noreply.github.com>
2023-03-12 20:42:18 +0300

    1-3
    pr2

---
+++ 1-3/3/Program.cs
@@ -0,0 +1,35 @@
+﻿class CesarShifr
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Введите текст, который нужно зашифровать");
+        string text = Console.ReadLine();
+        Console.WriteLine("Введите ключ");
+        int key = Convert.ToInt32(Console.ReadLine());
+        string ztext = z(text, key);
+        Console.WriteLine("Зашифрованный текст: " + ztext);
+        Console.WriteLine("Расшифрованый текст: " + rtext(ztext, key));
+        Console.ReadLine();
+
+    }
+
+    static string z(string text, int key)
+    {
+        string z = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            z += (char)(text[i] + key);
+        }
+        return z;
+    }
+
+    static string rtext(string text, int key)
+    {
+        string r = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            r += (char)(text[i] - key);
+        }
+        return r;
+    }
+}
