c79dfa5a3e8fb367be39b3162da66eb784da6f40
1schistad <125075398+1schistad@users.noreply.github.com>
2023-03-19 16:05:16 +0300

    pr4
    pr4

---
+++ ПР4/2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+class Posilka
+{
+    public string Description { get; private set; }
+    public int Weight { get; private set; }
+
+    public Posilka(string description, int weight)
+    {
+        Description = description;
+        Weight = weight;
+    }
+}
+
+class SendingService
+{
+    public int WeightLimit { get; private set; }
+
+    private const int Limit = 20;
+
+    public void SendPosilka(Posilka posilka)
+    {
+        WeightLimit += posilka.Weight;
+        if (WeightLimit >= Limit)
+        {
+            Console.WriteLine("Вес посылок превысил лимит, отправка отменена");
+        }
+        else
+        {
+            Console.WriteLine("Посылка {0} весом {1} кг успешно отправлена", posilka.Description, posilka.Weight);
+        }
+    }
+}
+
+class zadacha2
+{
+    static void Main()
+    {
+        Posilka posilkaName = new Posilka("'Телефон Ultra Super Hyper Xiamo Poko Pro 3'", 5);
+        SendingService sendingService = new SendingService();
+        sendingService.SendPosilka(posilkaName);
+        Console.ReadKey();
+    }
+}
---
+++ ПР4/3/Program.cs
@@ -0,0 +1,40 @@
+﻿class Ork
+{
+    public int Gold { get; private set; }
+
+    private static int CountGold;
+
+    public static int GoldAll { get; private set; }
+
+    public Ork()
+    {
+        CountGold++;
+        GoldAll += 2;
+        if (CountGold > 5)
+        {
+            Gold += 2;
+            GoldAll -= 2;
+        }
+    }
+
+}
+
+class zadacha3
+{
+    static void Main(string[] args)
+    {
+        Ork orc_1 = new Ork();
+        Console.WriteLine("Кол-во переносимого золота у 1-ого орка = " + Ork.GoldAll);
+        Ork orc_2 = new Ork();
+        Console.WriteLine("Кол-во переносимого золота у 2-ого орка = " + Ork.GoldAll);
+        Ork orc_3 = new Ork();
+        Console.WriteLine("Кол-во переносимого золота у 3-го орка = " + Ork.GoldAll);
+        Ork orc_4 = new Ork();
+        Console.WriteLine("Кол-во переносимого золота у 4-ого орка = " + Ork.GoldAll);
+        Ork orc_5 = new Ork();
+        Console.WriteLine("Кол-во переносимого золота у 5-ого орка = " + Ork.GoldAll);
+        Ork orc_6 = new Ork();
+        Console.WriteLine("Кол-во переносимого золота у 6-ого орка = " + orc_6.Gold);
+        Console.ReadKey(true);
+    }
+}
